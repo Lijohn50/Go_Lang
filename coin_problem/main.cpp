9abbae5fdbdd3108396af73484b667d0aeb2e4f0
Md. Mahfujur Rahman Lijon <152288455+Lijohn50@users.noreply.github.com>
2026-03-03 23:35:51 +0600

    Add files via upload

---
+++ coin_problem/main.cpp
@@ -0,0 +1,34 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int coin[] = {1, 3, 5};
+    int amount = 8;
+    int sol[(sizeof(coin) / sizeof(coin[0]))+1][amount+1];
+    
+    for(int i = 0; i < sizeof(coin) / sizeof(coin[0]); i++){
+        
+        for(int j = 0; j <= amount; j++){
+            
+            if(i == 0 && j == 0){
+                
+                sol[i][j] = 1;
+            }else{
+                
+                if(coin[i] < j){
+                    
+                    sol[i][j] = sol[i-1][j];
+                }else{
+                    
+                    sol[i][j] = sol[i - 1][j] + sol[i][j - coin[i]];
+                }
+            }
+        }
+    }
+    
+    cout << sol[3][8];
+    return 0;
+}
